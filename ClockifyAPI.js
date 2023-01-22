fb23c9c90cf8616f51b8cf0f8a5c8cbd1ec764cd
Andrew Jordaan <andrew.jordaan@platform45.com>
2023-01-22 08:29:18 +0200

    Load tasks and events from config files

--- ClockifyAPI.js
+++ ClockifyAPI.js
@@ -1,4 +1,5 @@
-import * as dotenv from 'dotenv' 
+import * as dotenv from 'dotenv'
+import fs from 'fs'
 dotenv.config()
 
 import got from 'got';
@@ -18,44 +19,7 @@ const USER_ID = process.env.CLOCKIFY_USER_ID
 
 const client = got.extend(options)
 
-const CLOCKIFY_TASKS = {
-  development: {
-    "id": "635bdc699264db257e9f1a28",
-    "name": "Development",
-    "projectId": "632835b815e8e969ad37c0c4",
-    "workspaceId": "61c04d29f526e061858f97c6"
-  },
-  clentMeeting: {
-    "id": "635bdc66046c243d2ea6cdd8",
-    "name": "Client Meeting",
-    "projectId": "632835b815e8e969ad37c0c4",
-    "workspaceId": "61c04d29f526e061858f97c6"
-  },
-  ceremony: {
-    "id": "635bdc63be208c7ada2a6ec2",
-    "name": "Ceremony",
-    "projectId": "632835b815e8e969ad37c0c4",
-    "workspaceId": "61c04d29f526e061858f97c6"
-  },
-  sickLeave: {
-    "id": "63515a2b9cc2917e2fc4176f",
-    "name": "Sick Leave",
-    "projectId": "635159c5ae317d4d60db7c29",
-    "workspaceId": "61c04d29f526e061858f97c6"
-  },
-  publicHoliday: {
-    "id": "63515a286377717db5d312f7",
-    "name": "Public Holiday",
-    "projectId": "635159c5ae317d4d60db7c29",
-    "workspaceId": "61c04d29f526e061858f97c6"
-  },
-  annualLeave: {
-    "id": "63515a1fd8923012ea8d0508",
-    "name": "Annual Leave",
-    "projectId": "635159c5ae317d4d60db7c29",
-    "workspaceId": "61c04d29f526e061858f97c6"
-  }
-}
+export const CLOCKIFY_TASKS = JSON.parse(fs.readFileSync('config/clockify-tasks.json'))
 
 export const addTimeEntryFor = async (taskType, start, end) => {
   return addTimeEntry(CLOCKIFY_TASKS[taskType], start, end)
@@ -71,15 +35,17 @@ export const addTimeEntry = async (task, start, end) => {
       end
     }
   }
-  const res = await client.post(url, options)
+  console.log('added time entry')
+  // const res = await client.post(url, options)
 }
 
 export const getTimeEntries = async (workspaceId, startTime, endTime) => {
 
   const url = `workspaces/${workspaceId}/user/${USER_ID}/time-entries?start=${startTime}&end=${endTime}`
 
-  const res = await client.get(url)
-  return JSON.parse(res.body)
+  return []
+  // const res = await client.get(url)
+  // return JSON.parse(res.body)
 }
 
 export const getTimeEntriesForToday = async (taskId, projectId, workspaceId) => {
