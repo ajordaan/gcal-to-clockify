869fb3e1af0fa3f1c105b536dc970c5058c52447
Andrew Jordaan <andrew.jordaan@platform45.com>
2023-01-27 17:02:44 +0200

    Add prompts library

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
 import { calendar, getEvents, getEventsForToday } from './GoogleCalendarAPI.js';
 import { addTimeEntryFor, getTimeEntries, CLOCKIFY_TASKS } from './ClockifyAPI.js'
 import fs from 'fs'
-import readline from 'readline'
+import prompts from 'prompts';
 import { getGapsInCalendarSchedule, currentWorkDay, WORK_DAY_END_TIME, WORK_DAY_START_TIME, title } from './Utils.js'
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
+const categoriseUnknownEvents = async (events, event_types, taskTypes) => {
 
-import yargs from 'yargs/yargs';
-
-const categoriseUnknownEvents = async (events, event_types) => {
-
-  console.log('categorising events')
-  const taskTypes = Object.keys(CLOCKIFY_TASKS)
-  taskTypes.push('ignore')
-
-  console.log({taskTypes})
-
+  const taskTypeChoices = taskTypes.sort().map(taskType => { return { title: taskType, value: taskType } })
   const unknownEvents = events.filter(event => !event_types[event.summary])
 
-  // console.log({unknownEvents})
+  const categorisedEventQuestions = unknownEvents.map(event => {
+    return {
+      type: 'select',
+      name: event.summary,
+      message: `What category is: ${event.summary}`,
+      choices: taskTypeChoices
+    }
+  })
 
+  const responses = await prompts(categorisedEventQuestions);
 
-  const categorised = {}
+  console.log(responses)
 
-  for (const event of unknownEvents) {
-    console.log(`What type of event is: ${event.summary}`)
-    
-    taskTypes.forEach((task, i) => console.log(`${i+1}. ${task}`) )
-
-    const typeIndex = await prompt("Type: ");
-
-    console.log('selected: ' + typeIndex)
-
-    categorised[event.summary] = taskTypes[typeIndex - 1]
-
-  }
-
-  return categorised
+  return responses
 }
 
 const saveCategorisedEvents = (events) => fs.writeFileSync('config/calendar-event-types.json', JSON.stringify(events))
+
+const timeInHoursMinutes = (date) => `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2,'0')}`
 
 const updateClockify = async (date) => {
   const targetDate = date
@@ -59,38 +46,40 @@ const updateClockify = async (date) => {
     return
   }
 
-  // const events = await getEvents(workDay.start, workDay.end)
-  const events = JSON.parse(fs.readFileSync('./google-calendar-events-stub.json'))
+  const events = await getEvents(workDay.start, workDay.end)
+  // const events = JSON.parse(fs.readFileSync('./google-calendar-events-stub.json'))
 
   if (events.length == 0) {
     console.error('No calendar events found');
     return;
   }
 
-  const taskTypes = Object.keys(CLOCKIFY_TASKS).map(task => task.name)
-  taskTypes.push('Ignore')
-
-
   let EVENT_TYPES = JSON.parse(fs.readFileSync('config/calendar-event-types.json'))
-  const newCategorisedEvents = await categoriseUnknownEvents(events, EVENT_TYPES)
+  const taskTypes = Object.keys(CLOCKIFY_TASKS)
+  taskTypes.push('ignore')
 
-  EVENT_TYPES = {...EVENT_TYPES, ...newCategorisedEvents}
+  const newCategorisedEvents = await categoriseUnknownEvents(events, EVENT_TYPES, taskTypes)
+
+  EVENT_TYPES = { ...EVENT_TYPES, ...newCategorisedEvents }
 
   saveCategorisedEvents(EVENT_TYPES)
 
   const IGNORED_EVENTS = Object.keys(EVENT_TYPES).filter(eventName => EVENT_TYPES[eventName] === 'ignore')
   const filteredEvents = events.filter(event => !IGNORED_EVENTS.includes(event.summary))
-
   const gaps = getGapsInCalendarSchedule(filteredEvents, workDay)
-  const developmentEvents = gaps.map(gap => { return { summary: 'Development', start: { dateTime: gap.start }, end: { dateTime: gap.end } } })
-  events.push(...developmentEvents)
+  const developmentEvents = gaps.map(gap => { return { summary: 'development', start: { dateTime: gap.start }, end: { dateTime: gap.end } } })
 
   try {
     filteredEvents.forEach(event => {
       if (EVENT_TYPES[event.summary]) {
         addTimeEntryFor(EVENT_TYPES[event.summary], new Date(event.start.dateTime).toISOString(), new Date(event.end.dateTime).toISOString())
-        console.log(`Added a ${EVENT_TYPES[event.summary]} entry for ${event.summary}`)
+        console.log(`Added a ${EVENT_TYPES[event.summary]} entry for ${event.summary} (${timeInHoursMinutes(new Date(event.start.dateTime))} - ${timeInHoursMinutes(new Date(event.end.dateTime))})`)
       }
+    })
+
+    developmentEvents.forEach(devEvent => {
+      addTimeEntryFor('development', new Date(devEvent.start.dateTime).toISOString(), new Date(devEvent.end.dateTime).toISOString())
+      console.log(`Added a development entry (${timeInHoursMinutes(new Date(devEvent.start.dateTime))} - ${timeInHoursMinutes(new Date(devEvent.end.dateTime))})`)
     })
   } catch (error) {
     console.error(error)
@@ -98,20 +87,33 @@ const updateClockify = async (date) => {
 
 }
 
-yargs(process.argv.slice(2))
-  .scriptName("Calendar to Clockify Uploader")
-  .usage('$0 <cmd> [args]')
-  .command('clock-in [date]', 'Upload calendar events as clockify time entries', (yargs) => {
-    yargs.positional('date', {
-      type: 'string',
-      default: null,
-      describe: 'The date you want to upload events from (YYYY-MM-DD)'
-    })
-  }, (argv) => {
-    const date = argv.date ? new Date(argv.date) : new Date()
-    console.log(title)
-    updateClockify(date)
-  })
-  .help()
-  .argv
+(async () => {
+  console.log(title)
+  const questions = [
+    {
+      type: 'select',
+      name: 'action',
+      message: 'Choose an action',
+      choices: [
+        { title: 'Clock In', value: 'clock-in' },
+        { title: 'Add custom time entry', value: 'custom-entry' },
+        { title: 'Setup', value: 'setup' }
+      ]
+    },
+    {
+      type: prev => prev == 'clock-in' ? 'text' : null,
+      name: 'clockInDate',
+      message: 'Enter clock-in date (YYYY-MM-DD). Leave blank for today\'s date'
+    }
+  ]
+  const response = await prompts(questions);
+
+  // console.log(response)
+
+  switch (response.action) {
+    case 'clock-in':
+      const date = response.clockInDate ? new Date(response.clockInDate) : new Date()
+      updateClockify(date)
+  }
+})();
 
