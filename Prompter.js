9d49069d1d89e4b572b310dd34dace3b2d07ed79
Andrew Jordaan <andrew.jordaan@platform45.com>
2023-02-15 15:27:31 +0200

    store prompts in file

---
+++ Prompter.js
@@ -0,0 +1,42 @@
+export const mainMenuPrompts = [
+  {
+    type: 'select',
+    name: 'action',
+    message: 'Choose an action',
+    choices: [
+      { title: 'Clock In', value: 'clock-in' },
+      { title: 'Add custom time entry', value: 'custom-entry' },
+      { title: 'Setup', value: 'setup' },
+      { title: 'Exit', value: 'exit' }
+    ]
+  },
+  {
+    type: prev => prev == 'clock-in' ? 'text' : null,
+    name: 'clockInDate',
+    message: 'Enter clock-in date (YYYY-MM-DD). Leave blank for today\'s date'
+  }
+]
+
+export const multiSelectPrompt = ({ name, message, choices, maxSelection, hint }) => {
+  return {
+    type: 'multiselect',
+    name,
+    message,
+    choices,
+    max: maxSelection,
+    hint,
+    instructions: false
+  }
+}
+
+export const numberPrompt = ({name, message, initial, min, max}) => {
+  return {
+    type: 'number',
+    name,
+    message,
+    initial,
+    style: 'default',
+    min,
+    max
+  }
+}
